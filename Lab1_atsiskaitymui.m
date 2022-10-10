041f6cc30186146a4aa2c93b989da3a10b74ef3d
LaimaIeva <113528956+LaimaIeva@users.noreply.github.com>
2022-10-10 20:00:04 +0300

    Update and rename IS_lab_1_template.m to Lab1_atsiskaitymui.m
    Kodas atsiskaitymui, kuomet pakeisti naudojami duomenys (obuoliai 6, 7; kriaušės 2, 3, 4).

--- Lab1_atsiskaitymui.m
+++ Lab1_atsiskaitymui.m
@@ -1,5 +1,4 @@
 % Classification using perceptron
-
 % Reading apple images
 A1=imread('apple_04.jpg');
 A2=imread('apple_05.jpg');
@@ -10,7 +9,6 @@ A6=imread('apple_12.jpg');
 A7=imread('apple_13.jpg');
 A8=imread('apple_17.jpg');
 A9=imread('apple_19.jpg');
-
 % Reading pears images
 P1=imread('pear_01.jpg');
 P2=imread('pear_02.jpg');
@@ -56,20 +54,20 @@ metric_P2=apvalumas_roundness(P2); %roundness
 %3rd pear image(P3)
 hsv_value_P3=spalva_color(P3); %color
 metric_P3=apvalumas_roundness(P3); %roundness
-%2nd pear image(P4)
+%4th pear image(P4)
 hsv_value_P4=spalva_color(P4); %color
 metric_P4=apvalumas_roundness(P4); %roundness
 
 %selecting features(color, roundness, 3 apples and 2 pears)
 %A1,A2,A3,P1,P2
 %building matrix 2x5
-x1=[hsv_value_A1 hsv_value_A2 hsv_value_A3 hsv_value_P1 hsv_value_P2];
-x2=[metric_A1 metric_A2 metric_A3 metric_P1 metric_P2];
+x1=[hsv_value_A6 hsv_value_A7 hsv_value_P2 hsv_value_P3 hsv_value_P4];
+x2=[metric_A6 metric_A7 metric_P2 metric_P3 metric_P4];
 % estimated features are stored in matrix P:
 P=[x1;x2];
 
 %Desired output vector
-T=[1;1;1;-1;-1]; % <- ČIA ANKSČIAU BUVO KLAIDA!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+d=[1;1;-1;-1;-1]; % <- ČIA ANKSČIAU BUVO KLAIDA!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 %% train single perceptron with two inputs and one output
 
@@ -79,52 +77,166 @@ w2 = randn(1);
 b = randn(1);
 
 % calculate weighted sum with randomly generated parameters
-%v1 = <...>; % write your code here
 % calculate current output of the perceptron 
+% calculate the error
+v1 = x1(1)*w1+x2(1)*w2+b;
 if v1 > 0
 	y = 1;
 else
 	y = -1;
 end
-% calculate the error
-e1 = T(1) - y;
+e1 = d(1) - y;
 
-% repeat the same for the rest 4 inputs x1 and x2
-% calculate wieghted sum with randomly generated parameters
-% v2 = <...> ; % write your code here
-% calculate current output of the perceptron 
+v2 = x1(2)*w1+x2(2)*w2+b;
 if v2 > 0
 	y = 1;
 else
 	y = -1;
 end
-% calculate the error
-e2 = T(2) - y;
+e2 = d(2) - y;
 
-% <...> write the code for another 3 inputs
+v3 = x1(3)*w1+x2(3)*w2+b;
+if v3 > 0
+	y = 1;
+else
+	y = -1;
+end
+e3 = d(3) - y;
+
+v4 = x1(4)*w1+x2(4)*w2+b;
+if v4 > 0
+	y = 1;
+else
+	y = -1;
+end
+e4 = d(4) - y;
+
+v5 = x1(5)*w1+x2(5)*w2+b;
+if v5 > 0
+	y = 1;
+else
+	y = -1;
+end
+e5 = d(5) - y;
 
 % calculate the total error for these 5 inputs 
 e = abs(e1) + abs(e2) + abs(e3) + abs(e4) + abs(e5);
 
 % write training algorithm
 while e ~= 0 % executes while the total error is not 0
-	% here should be your code of parameter update
-%   calculate output for current example
-% 
-%   calculate error for current example
-% 
+% calculate weighted sum with randomly generated parameters
+% calculate current output of the perceptron
+% calculate the error
+v1 = x1(1)*w1+x2(1)*w2+b;
+if v1 > 0
+	y = 1;
+else
+	y = -1;
+end
+e1 = d(1) - y;
+
+v2 = x1(2)*w1+x2(2)*w2+b;
+if v2 > 0
+	y = 1;
+else
+	y = -1;
+end
+e2 = d(2) - y;
+
+v3 = x1(3)*w1+x2(3)*w2+b;
+if v3 > 0
+	y = 1;
+else
+	y = -1;
+end
+e3 = d(3) - y;
+
+v4 = x1(4)*w1+x2(4)*w2+b;
+if v4 > 0
+	y = 1;
+else
+	y = -1;
+end
+e4 = d(4) - y;
+
+v5 = x1(5)*w1+x2(5)*w2+b;
+if v5 > 0
+	y = 1;
+else
+	y = -1;
+end
+e5 = d(5) - y;
+
 %   update parameters using current inputs ant current error
-% 	w1 = 
-%   w2 = 
-%   b = 
-% 
+eta=0.15;
+w1 = w1+eta*e1*x1(1);
+w2 = w2+eta*e1*x2(1);
+b  = b+eta*e1;
+
+w1 = w1+eta*e2*x1(2);
+w2 = w2+eta*e2*x2(2);
+b  = b+eta*e2;
+
+w1 = w1+eta*e3*x1(3);
+w2 = w2+eta*e3*x2(3);
+b  = b+eta*e3;
+
+w1 = w1+eta*e4*x1(4);
+w2 = w2+eta*e4*x2(4);
+b  = b+eta*e4;
+
+w1 = w1+eta*e5*x1(5);
+w2 = w2+eta*e5*x2(5);
+b  = b+eta*e5;
+
 %   Test how good are updated parameters (weights) on all examples used for training
 %   calculate outputs and errors for all 5 examples using current values of the parameter set {w1, w2, b}
 %   calculate 'v1', 'v2', 'v3',... 'v5'
-% 
+
+    v1 = x1(1)*w1+x2(1)*w2+b;
+    v2 = x1(2)*w1+x2(2)*w2+b;
+    v3 = x1(3)*w1+x2(3)*w2+b;
+    v4 = x1(4)*w1+x2(4)*w2+b;
+    v5 = x1(5)*w1+x2(5)*w2+b;
+
 %   calculate 'y1', ..., 'y5'
-%     
+
+    if v1 > 0
+	    y(1) = 1;
+    else
+	    y(1) = -1;
+    end
+
+    if v2 > 0
+	    y(2) = 1;
+    else
+	    y(2) = -1;
+    end
+
+    if v3 > 0
+	    y(3) = 1;
+    else
+	    y(3) = -1;
+    end
+
+    if v4 > 0
+	    y(4) = 1;
+    else
+	    y(4) = -1;
+    end
+
+    if v5 > 0
+	    y(5) = 1;
+    else
+	    y(5) = -1;
+    end
+
 %   calculate 'e1', ... 'e5'
+    e1 = d(1) - y(1);
+    e2 = d(2) - y(2);
+    e3 = d(3) - y(3);
+    e4 = d(4) - y(4);
+    e5 = d(5) - y(5);
     
 	% calculate the total error for these 5 inputs 
 	e = abs(e1) + abs(e2) + abs(e3) + abs(e4) + abs(e5);
